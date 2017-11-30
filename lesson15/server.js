4dfa704c2c87e75da9b4befda7c75b8d63363b4a
ghostchan <jinnchan@qq.com>
2017-11-30 20:59:17 +0800

    lesson15 文件上传

--- lesson15/server.js
+++ lesson15/server.js
@@ -9,10 +9,23 @@ const jade=require('jade');
 var server=express();
 server.listen(8080);
 
+//解析cookie
 server.use(cookieParser('hhuuugfddd'));
 
+//2、使用session
 var arr=[];
 for(var i=0;i<100000;i++){
     arr.push('keys_'+Math.random());
 }
 server.use(cookieSession({name:'sess_id',keys:arr,maxAge:20*3600*1000}));
+
+//3、post数据
+server.use(bodyParser.urlencoded({extended:false}));
+
+//用户请求
+server.use('/',function(req,res,next){
+    console.log(req.query,req.body,req.cookies,req.session);
+});
+
+//4、static数据
+server.use(static('./www'));
